04fe1b99e8ceeac1bb501e978f186e90541b0203
renorram <renorramsilva@gmail.com>
2019-09-10 10:32:54 -0300

    Extraindo arquivos do modulo da yapay

---
+++ app/code/community/Tray/CheckoutApi/Block/Form/Standard.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to suporte@tray.net.br so we can send you a copy immediately.
+ *
+ * @category   Tray
+ * @package    Tray_CheckoutApi
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Tray_CheckoutApi_Block_Form_Standard extends Mage_Payment_Block_Form
+{
+    protected function _construct()
+    {
+        $this->setTemplate('tray/checkoutapi/form/standard.phtml');
+        parent::_construct();
+    }
+    protected function _getConfig()
+    {
+        return Mage::getSingleton('payment/config');
+    }
+    public function getCcMonths()
+    {
+        $months = $this->getData('cc_months');
+        if (is_null($months)) {
+            $months[0] =  $this->__('Month');
+            $months = array_merge($months, $this->_getConfig()->getMonths());
+            $this->setData('cc_months', $months);
+        }
+        return $months;
+    }
+    
+    public function getCcYears()
+    {
+        $years = $this->getData('cc_years');
+        if (is_null($years)) {
+            $years = $this->_getConfig()->getYears();
+            $years = array(0=>$this->__('Year'))+$years;
+            $this->setData('cc_years', $years);
+        }
+        return $years;
+    }
+}
---
+++ app/code/community/Tray/CheckoutApi/Block/Info/Onlinetransfer.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to suporte@tray.net.br so we can send you a copy immediately.
+ *
+ * @category   Tray
+ * @package    Tray_CheckoutApi
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Tray_CheckoutApi_Block_Info_Onlinetransfer extends Mage_Payment_Block_Info
+{
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('tray/checkoutapi/info/standard.phtml');
+    }
+        
+    public function getLinkPayment($order) 
+    {
+        if ($this->getRequest()->getRouteName() != 'checkout') {
+            $_order = $order;
+            $incrementid = $_order->getData('increment_id');
+            $quoteid = $_order->getData('quote_id');
+		
+            $hash = Mage::getModel('core/encryption')->encrypt($incrementid . ":" . $quoteid);
+            $method = $_order->getPayment()->getMethod();
+            
+            //Mage::log($_order->getCustomerId(), null, 'traycheckout.log');
+            //Mage::log(Mage::getModel('checkoutapi/payment')->getPayment(), null, 'traycheckout.log');
+            //Mage::log('Chamada: ', null, 'traycheckout.log');
+            
+            if ($method == "traycheckoutapi_onlinetransfer" && (($_order->getStatus() == Mage_Sales_Model_Order::STATE_PENDING_PAYMENT ) || ($_order->getStatus() == 'pending' ))){
+                return '<span><button onclick="window.open(\''.$_order->getPayment()->getData('traycheckout_url_payment').'\');" class="button btn-proceed-checkout btn-checkout" title="Efetuar Transfer&ecirc;ncia Online" type="button"><span><span>Efetuar Transfer&ecirc;ncia Online</span></span></button></span>';
+            }
+        }
+    }
+    
+    protected function _prepareSpecificInformation($transport = null)
+    {
+        if (null !== $this->_paymentSpecificInformation) {
+            return $this->_paymentSpecificInformation;
+        }
+        $info = $this->getInfo();
+        $transport = new Varien_Object();
+        $transport = parent::_prepareSpecificInformation($transport);
+        $transport->addData(array(
+            Mage::helper('payment')->__('Payment Method ID') => $info->getTraycheckoutTransactionId(),
+            Mage::helper('payment')->__('Split Number') => $info->getTraycheckoutSplitNumber(),
+            Mage::helper('payment')->__('Split Value') => $info->getTraycheckoutSplitValue()
+        ));
+        return $transport;
+    }
+
+}
